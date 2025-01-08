fb9a7275f29bd6dff6a2abb51696a7369b8038ad
sdijhegwkh <sprout890@gmail.com>
2025-01-08 10:20:44 +0700

    Hoan thanh part 1 cau 1

---
+++ NguyenThanhTu_22691321/index.js
@@ -0,0 +1,24 @@
+var massMark=78;
+var heightMark=1.69;
+var massJohn=92;
+var heightJohn=1.95;
+var caculateMark=(massMark/(heightMark**2));
+var caculateJohn=(massJohn/(heightJohn**2));
+var markHigherBMI=true;
+console.log("BMI cua mark trong test 1 la:",caculateMark);
+console.log("BMI cua mark trong test 1 la:",caculateJohn);
+if (caculateMark<=caculateJohn){
+    var markHigherBMI=false;}
+console.log("Gia tri cua markHigherBMI la:",markHigherBMI);
+var massMark=95;
+var heightMark=1.88;
+var massJohn=85;
+var heightJohn=1.76;
+var caculateMark=(massMark/(heightMark**2));
+var caculateJohn=(massJohn/(heightJohn**2));
+var markHigherBMI=true;
+console.log("BMI cua mark trong test 2 la:",caculateMark);
+console.log("BMI cua mark trong test 2 la:",caculateJohn);
+if (caculateMark<=caculateJohn){
+    var markHigherBMI=false;}
+console.log("Gia tri cua markHigherBMI la:",markHigherBMI);
